31a4e4e7a572a85ec3ab211da3fa940d1c880be7
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-16 19:18:49 +0300

    created classes covered tasks

---
+++ src/app/custom/Entity.js
@@ -0,0 +1,7 @@
+export default class Entity {
+    constructor(name, data) { // towa 6e vidim dali trq da e taka
+        this.name = name;
+        this.data = data;
+        // tuk trqbwa da wkaram wsi4ko ot root elementa i da go nabiq kato danni.
+    }
+};
